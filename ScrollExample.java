099d123d4d8a18a7e1cc719f71b599b6dbed77f0
Aayushstha1 <sudipk339@gmail.com>
2025-12-16 07:46:56 +0545

    New

--- ScrollExample.java
+++ ScrollExample.java
@@ -7,33 +7,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ScrollExample {
-
-    public static void main(String[] args) {
-           String url = "jdbc:mysql://localhost:3306/java2025";
+    private
+       String url = "jdbc:mysql://localhost:3306/java2025";
         String username = "root";
         String password = "";
         Connection conn = null;
         Statement st = null;
         Resulset rs = null;
-         
-        try{
-            conn = DriverManager.getConnection(url, username, password);
-            st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
-            String query = "SELECT id, name , salary, branch FROM employee";
+        public
+        ScrollExample(){
+            
+        try{
+            conn = DriverManager.getConnection(url, username, password); 
+          st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+           String query = "SELECT id, name , salary, branch FROM employee";
             rs = st.executeQuery(query);
-            rs.last();
-            System.out.println("Last Row: ID = " rs.getInt("id")+ 
+
+
+        } catch(SQLException ex){
+            System.out.println(ex);
+        }
+         void lastRow {
+            try{
+                rs.last();
+                 System.out.println("Last Row: ID = " rs.getInt("id")+ 
             ", Name = " + rs.getString("name")+
             ", Salary = " + rs.getDouble("salary")+
-            ", branch = " + rs.getString("branch")
+            ", branch = " + rs.getString("branch"));
 
-        );
-        }catch(SQLException ex){
-            System.out.println(ex);
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+        
+        }
+
+        public static void main(String[] args) {
+            ScrollExample s = new ScrollExample();
+            s.lastRow();
+        }
+        
         }
 
         
     }
+
     
-}
+
