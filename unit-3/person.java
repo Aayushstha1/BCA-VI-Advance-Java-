b0262a7d8b2d72114d26f3971b1cdc733c6490b5
Aayushstha1 <sudipk339@gmail.com>
2025-12-23 07:10:01 +0545

    New

---
+++ unit-3/person.java
@@ -0,0 +1,32 @@
+import java.io.Serializable;
+
+public class person implements Serializable {
+    private String name;
+    private int age;
+
+    public person() {
+    }
+
+    public person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+   
+}
